5e25c358abb840592c635870e964fcbc3b847b31
divas regmi <divasregmi@gmail.com>
2021-04-07 12:38:23 +0545

    data preprocessing done

--- Data processing/data_preprocessing.py
+++ Data processing/data_preprocessing.py
@@ -1,25 +1,26 @@
-# Data Preprocessing Template
-
 # Importing the libraries
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.impute import SimpleImputer
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
+
+# matrix of feature
 X = dataset.iloc[:, :-1].values
+# depedent variable vector
 Y = dataset.iloc[:, -1].values
 
 
 # Taking care of missing Data
+from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 X[:, 1:3] = imputer.fit_transform(X[:, 1:3])
 
 
 # Encoding the chategorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 # Encode Country Column (independent variable)
 labelencoder_X = LabelEncoder()
@@ -33,5 +34,11 @@ LabelEncoder_Y = LabelEncoder()
 Y = LabelEncoder_Y.fit_transform(Y)
 
 # Splitting the dataset into the Training set and Test set
-# from sklearn.model_selection import train_test_split
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+# Feature scaling 
+from sklearn.preprocessing import  StandardScaler
+sc = StandardScaler()
+X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = sc.transform(X_test[:, 3:])
---
+++ data_preprocessing_template.py
@@ -0,0 +1,22 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=0)
+
+# Feature scaling
+'''from sklearn.preprocessing import  StandardScaler
+sc = StandardScaler()
+X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = sc.transform(X_test[:, 3:])'''
